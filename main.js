e14b7da05d2e7a32c56fbb577f5697f9be8614d6
Pedro Magossi Santos <pedroms.pms@gmail.com>
2022-04-02 21:29:53 -0300

    :sparkles: Add ScoreBoard in main

--- main.js
+++ main.js
@@ -3,18 +3,16 @@ import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
 
 import BordGame from './src/object/BoardGame'
-import PlayerName from './src/components/PlayerName'
+
+import ScoreBoard from './src/object/ScoreBoard'
 
 
 const $root = document.querySelector("#root")
-const $htmlBordGame = BordGame(6)
-const $htmlPLayerName = PlayerName()
 
 $root.insertAdjacentHTML(
     "beforeend",
     `
-    ${PlayerName("PLayer 1")}
-    ${PlayerName("PLayer 2")}
+    ${ScoreBoard}
     ${BordGame(6)}
     `
 )
