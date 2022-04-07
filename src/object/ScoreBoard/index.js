866fb5a39316cdb0bda0206a86855a187984bef3
Pedro Magossi Santos <pedroms.pms@gmail.com>
2022-04-06 21:18:51 -0300

    :construction: Create Html of the PlayerScore componet

--- src/object/ScoreBoard/index.js
+++ src/object/ScoreBoard/index.js
@@ -1,4 +1,5 @@
 import PlayerName from "../../components/PlayerName"
+import PlayerScore from "../../components/PlayerScore"
 import VsPlayer from "../../components/VsPayer"
 import "./style.css"
 
@@ -7,7 +8,9 @@ function ScoreBoard(){
     return /*html*/ `
     <header class="score-board">
     ${PlayerName("PLayer 1")}
+    ${PlayerScore(1)}
     ${VsPlayer()}
+    ${PlayerScore()}
     ${PlayerName("PLayer 2")}
     </header>
     `
